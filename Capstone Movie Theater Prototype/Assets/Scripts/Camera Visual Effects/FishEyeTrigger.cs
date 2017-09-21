0b2f5cac9b2c987335fbde0a358f068844b7431b
3voker <Drechristophe@gmail.com>
2017-09-21 16:51:00 -0500

    Push?

---
+++ Capstone Movie Theater Prototype/Assets/Scripts/Camera Visual Effects/FishEyeTrigger.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class FishEyeTrigger : MonoBehaviour
+{
+
+    // Use this for initialization
+    Fisheye fEye;
+    float timerfEye = 5.0f;
+
+    //GameObject game
+    //   public GameObject;
+    void Start()
+    {
+
+        fEye = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Fisheye>();
+        fEye.enabled = false;
+
+    }
+    void Update()
+    {
+        if (fEye.enabled == true)
+        {
+            timerfEye -= Time.deltaTime;
+        }
+        if (timerfEye < 0)
+        {
+            timerfEye = 5;
+            fEye.enabled = false;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            fEye.enabled = true;
+        }
+    }
+
+
+}
+
+
+
+
